7a94bef4304c33e688df9ef2f599ed31d1a88e49
Fatih <16fay61@gmail.com>
2023-07-16 17:13:58 +0200

    aa

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,37 +1,70 @@
-import React, { useEffect } from 'react';
+import React, { useState,useEffect } from 'react';
 import { useSelector } from "react-redux";
 import useBlogCalls from "../hooks/useBlogCalls";
 import { Grid } from '@mui/material';
 import { flexCenter } from "../styles/globalStyle";
 import BlogCard from '../components/blog/BlogCard';
 import { Helmet } from "react-helmet";
-import loadingGif from "../assets/loading1.gif";
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 
 
 const Dashboard = () => {
   const { getBlogDataPublic } = useBlogCalls();
-  const { blogs, loading } = useSelector(state => state.blog);
+  const { blogs } = useSelector(state => state.blog);
+  const [scrollToTop, setScrollToTop] = useState(false);
+
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
 
   useEffect(() => {
-    getBlogDataPublic("blogs");
+    getBlogDataPublic('blogs');
     console.log(blogs);
+
+    const handleScroll = () => {
+      if (window.scrollY > 200) {
+        setScrollToTop(true);
+      } else {
+        setScrollToTop(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
   return (
     <div>
       <Grid container sx={flexCenter}>
         <Helmet>
           <title>Dashboard</title>
         </Helmet>
-        {loading ? (
-          <img src={loadingGif} alt="loading..." style={{ height: '300px', margin: "140px" }} />
-        ) : (
-          blogs?.map(blog => (
-            <Grid sx={{ mt: 3, mb: 3 }} item key={blog.id}>
-              <BlogCard blog={blog} />
-            </Grid>
-          )))}
+        {blogs?.map(blog => (
+          <Grid sx={{ mt: 3, mb: 3 }} item key={blog.id}>
+            <BlogCard blog={blog} />
+          </Grid>
+        ))}
       </Grid>
+      {scrollToTop && (
+        <button
+          onClick={handleScrollToTop}
+          style={{
+            position: 'fixed',
+            bottom: '15px',
+            right: '12px',
+            padding: '7px',
+            fontSize: '16px',
+            backgroundColor:"lightgray",
+            borderRadius:"5px",
+          }}
+        >
+          <ArrowUpwardIcon/>
+        </button>
+      )}
     </div>
   )
 }
